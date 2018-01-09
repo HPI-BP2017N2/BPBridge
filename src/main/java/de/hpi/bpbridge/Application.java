95899e8ee0e7da9bec7bcc045e69a1dac9bf588a
deeps96 <leonardo.huebscher.business@gmail.com>
2018-01-09 11:38:38 +0100

    Integrated maven and spring.

---
+++ src/main/java/de/hpi/bpbridge/Application.java
@@ -0,0 +1,12 @@
+package de.hpi.bpbridge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+}
